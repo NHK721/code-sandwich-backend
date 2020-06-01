70da3ce72cacb8be9ac2f74b39baded30fba1c38
Woo Hyun An <nickanism@gmail.com>
2020-06-01 11:10:17 +0900

    ADD added all the default data (not user-created) some customization GET views were created

--- db_uploader.py
+++ db_uploader.py
@@ -6,17 +6,20 @@ import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codesandwich.settings")
 django.setup()
 
-from product.models   import Category
+from product.models   import IngredientCategory, Ingredient, Product, SubCategory, Category, Nutrition, PlaceOfOrigin, Allergen, IngredientPlaceOfOrigin, IngredientAllergen
 
-CSV_PATH_STORES = './categories.csv'
+CSV_PATH_STORES = './allergens_ingredients.csv'
 
-def insert_stores():
+def insert_data():
     with open(CSV_PATH_STORES) as in_file:
         data_reader = csv.reader(in_file)
         next(data_reader, None)
         for row in data_reader:
-            name = row[0]
+            allergen_id = row[0]
+            ingredient_id = row[1]
+            ingredient = Ingredient.objects.get(id=ingredient_id)
+            allergen = Allergen.objects.get(id=allergen_id)
 
-            Category.objects.create(name = name)
+            IngredientAllergen.objects.create(ingredient = ingredient, allergen = allergen)
 
-insert_stores()
+insert_data()
--- product/views.py
+++ product/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+import json
+import ast
 
-# Create your views here.
+from django.views               import View
+from django.http                import JsonResponse
+
+from .models import Product, Category, SubCategory, Nutrition, Ingredient, ProductIngredient
+
+class ProductView(View):
+    def get(self, request):
+        product_id = request.GET.get('product_id', None)
+        product_data = Product.objects.filter(id=product_id).all().values()[0]
+        nutrition_data = Nutrition.objects.filter(id = product_id).all().values()[0]
+        return JsonResponse({
+            "product":product_data,
+            "nutirion":nutrition_data,
+        }, status=200)
+
+class SandwichView(View):
+    def get(self, request):
+        sandwiches = Product.objects.filter(category_id=1).values()
+        return JsonResponse({'sandwiches':list(sandwiches)}, status=200)
+
+class CustomizationView(View):
+    def get(self, request):
+        product_id = request.GET.get('product_id', None)
+        default_ingredients = ProductIngredient.objects.filter(is_default=True).filter(product_id = product_id)
+        lst = []
+        lst2 = []
+        for ingredient in default_ingredients.values():
+            lst.append(ingredient['ingredient_id'])
+        for ingredient_id in lst:
+            lst2.append(Ingredient.objects.filter(id=ingredient_id).all().values()[0])
+        for bread in lst2:
+            if "bottom" in bread['name']:
+                lst2.append(lst2.pop(lst2.index(bread)))
+                break
+        return JsonResponse({'default_ingredients':lst2})
+
+class ToppingView(View):
+    def get(self, request):
+        product_id = request.GET.get('product_id', None)
+        default_ingredients = ProductIngredient.objects.filter(is_default=True).filter(product_id = product_id)
+        lst = []
+        lst2 = []
+        topping_lst = []
+        for ingredient in default_ingredients.values():
+            lst.append(ingredient['ingredient_id'])
+        for ingredient_id in lst:
+            lst2.append(Ingredient.objects.filter(id=ingredient_id).all().values()[0])
+        for i in lst2:
+            if i["ingredient_category_id"] != 1:
+                topping_lst.append(i)
+        return JsonResponse({'default_toppings': topping_lst})
+
+class BreadView(View):
+    def get(self, request):
+        product_id = request.GET.get('product_id', None)
+        default_ingredients = ProductIngredient.objects.filter(is_default=True).filter(product_id = product_id)
+        lst = []
+        lst2 = []
+        bread_lst = []
+        for ingredient in default_ingredients.values():
+            lst.append(ingredient['ingredient_id'])
+        for ingredient_id in lst:
+            lst2.append(Ingredient.objects.filter(id=ingredient_id).all().values()[0])
+        for i in lst2:
+            if i["ingredient_category_id"] == 1:
+                bread_lst.append(i)
+        return JsonResponse({'default_bread': bread_lst})
+
+    def post(self, request, *args, **kwargs):
+        body_unicode = request.body.decode('utf-8')
+        body = ast.literal_eval(body_unicode)
+        bread_id = body['bread']
+        bread_lst = []
+        bread_id_lst = bread_id.split(',')
+        for i in bread_id_lst:
+            bread_lst.append(Ingredient.objects.filter(id=i).values()[0])
+
+        return JsonResponse({'changed_bread': bread_lst})
