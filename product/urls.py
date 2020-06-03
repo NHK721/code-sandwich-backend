44ef6a7fde9cedf01970f7cba25ea3597c815188
Woo Hyun An <nickanism@gmail.com>
2020-06-03 18:26:37 +0900

    [ADD] added GET views for wraps

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,19 @@
 from django.urls        import path, re_path
-from .views             import ProductView, SandwichView, CustomizationView, ToppingView, BreadView
+from .views             import (
+    ProductView, 
+    SandwichView, 
+    CustomizationView, 
+    ToppingView, 
+    BreadView, 
+    WrapView,
+    WrapDetailView
+)
 
 urlpatterns = [
     re_path(r'^/sandwich/(?:\?product_id=(?P<product_id>\d+)/)?$', ProductView.as_view()),
+    re_path(r'^/wrap/(?:\?product_id=(?P<product_id>\d+)/)?$', WrapDetailView.as_view()),
     path('/sandwich', SandwichView.as_view()),
+    path('/wrap', WrapView.as_view()),
     re_path(r'^/sandwich/customization/(?:\?product_id=(?P<product_id>\d+)/)?$', CustomizationView.as_view()),
     re_path(r'^/sandwich/customization/topping/(?:\?product_id=(?P<product_id>\d+)/)?$', ToppingView.as_view()),
     re_path(r'^/sandwich/customization/bread/(?:\?product_id=(?P<product_id>\d+)/)?$', BreadView.as_view()),
--- product/views.py
+++ product/views.py
@@ -8,6 +8,7 @@ from django.http                import JsonResponse
 from django.db.models           import Q
 from django.forms.models        import model_to_dict
 from django.core                import serializers
+from django.shortcuts           import redirect, render
 
 from .models import (
     Product, 
@@ -63,6 +64,7 @@ class CustomizationView(View):
             default_ingredients = ProductIngredient.objects.filter(is_default=True, product_id = product_id)
             lst = []
             lst2 = []
+            all_bread = [bread for bread in Ingredient.objects.filter(ingredient_category_id=1).values()]
             for ingredient in default_ingredients.values():
                 lst.append(ingredient['ingredient_id'])
             for ingredient_id in lst:
@@ -71,7 +73,7 @@ class CustomizationView(View):
                 if "bottom" in bread['name']:
                     lst2.append(lst2.pop(lst2.index(bread)))
                     break
-            return JsonResponse({'default_ingredients':lst2, 'product_name':product_name})
+            return JsonResponse({'default_ingredients':lst2, 'product_name':product_name, 'all_bread': all_bread})
         return JsonResponse({'message':"You can't customize this item"})
 
 class ToppingView(View):
@@ -119,14 +121,44 @@ class BreadView(View):
             if i["ingredient_category_id"] == 1:
                 bread_lst.append(i)
         return JsonResponse({'default_bread': bread_lst, 'all_bread': all_bread})
+    
+    def post(self, request):
+        product_id = request.GET.get('product_id', None)
+        body = ast.literal_eval(request.body.decode('utf-8'))
+        breads = [item['id'] for item in body]
+        bread1 = breads[0]
+        bread2 = breads[1]
+        response = redirect('/product/sandwich/customization/?product_id=' + product_id)
+        response.set_cookie('chosen_bread1', bread1)
+        response.set_cookie('chosen_bread2', bread2)
+        return response
 
-    def post(self, request, *args, **kwargs):
-        body_unicode = request.body.decode('utf-8')
-        body = ast.literal_eval(body_unicode)
-        bread_id = body['bread']
-        bread_lst = []
-        bread_id_lst = bread_id.split(',')
-        for i in bread_id_lst:
-            bread_lst.append(Ingredient.objects.filter(id=i).values()[0])
+class WrapView(View):
+    def get(self, request):
+        wraps = Product.objects.filter(category_id=2).values()
+        return JsonResponse({'wraps':list(wraps)}, status=200)
 
-        return JsonResponse({'changed_bread': bread_lst})
+class WrapDetailView(View):
+    def get(self, request):
+        product_id = request.GET.get('product_id', None)
+        product_info = Product.objects.filter(id=product_id).values()[0]
+        product = Product.objects.get(id=product_id)
+        nutrition = product.nutrition
+        nutrition = model_to_dict(nutrition)
+        product_group = product.subcategory
+        group_products = list(Product.objects.filter(subcategory = product_group, category = 2).values())
+        show_products = []
+
+        for i in group_products:
+            try:
+                if i == product_info:
+                    show_products.append(group_products[group_products.index(i) + 1])
+            except IndexError:
+                show_products.append(group_products[0])
+        show_products = list(show_products)
+
+        return JsonResponse({
+            "product"                   : product_info,
+            "nutrition"                 : nutrition,
+            "all_subcategory_products"  : show_products,
+        }, status=200)
