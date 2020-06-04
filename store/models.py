322c9f40458a17869929dc111cb43ab9787b5dbd
NHK <aptx4869.nhk@gmail.com>
2020-06-04 12:19:09 +0900

    [ADD] store views.py 작성 [FIX] store models.py (province 추가)

--- store/models.py
+++ store/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
 class Store(models.Model):
-    address     = models.CharField(max_length=200)
-    name        = models.CharField(max_length=100)
-    phone       = models.CharField(max_length=20, unique=True)
-
+    no          = models.IntegerField(null=True)
+    address     = models.CharField(max_length=200, null = True)
+    name        = models.CharField(max_length=100, null = True)
+    phone       = models.CharField(max_length=20, null = True)
+    time        = models.CharField(max_length = 50, null = True)
+    longitude   = models.DecimalField(max_digits = 50, decimal_places = 7, null = True)
+    latitude    = models.DecimalField(max_digits = 50, decimal_places = 7, null = True)
+    province    = models.ForeignKey('Province', on_delete = models.CASCADE, null=True)
     class Meta:
         db_table = 'stores'
 
     def __str__(self):
-        return "store address: " + address + "store phone number: " + phone
+        return self.name
+
+class Province(models.Model):
+    name    = models.CharField(max_length = 50)
+
+    class Meta:
+        db_table = 'provinces'
+
+    def __str__(self):
+        return self.name
+                
--- store/views.py
+++ store/views.py
@@ -1,3 +1,82 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import HttpResponse, JsonResponse
+from django.db.models import Q
+from .models import Store
+
+
+class StoreView(View):
+    def get(self, request):
+
+        offset = int(request.GET.get('offset',0))
+        limit = int(request.GET.get('limit',10))
+
+        try:
+            stores = Store.objects.all()
+            main_store = [
+                {
+                    'id' : store.id,
+                    'no' : store.no,
+                    'name' : store.name,
+                    'address' : store.address,
+                    'phone': store.phone
+                }for store in stores[offset * limit : (offset+1) * limit]
+            ]
+            total = stores.count()
+            return JsonResponse({'stores' : main_store, 'total' : total }, status=200)
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEY'}, status=400)
+
+
+class OpenView(View):
+    def get(self, request):
+
+        try:
+            stores = Store.objects.filter(phone="Coming Soon")
+
+            open_store = [
+                 {
+                         'province' : store.province.name,
+                         'name' : store.name,
+                         'phone' : store.phone
+                 } for store in stores
+              ]
+            return JsonResponse({'stores' : open_store}, status=200)
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEY'}, status=400)
+
+class DetailView(View):
+    def get(self, request, store_id):
+
+        store = Store.objects.get(id = store_id)
+
+        store_data = [
+                {
+                    'id' : store.id,
+                    'name' : store.name,
+                    'address' : store.address,
+                    'phone' : store.phone,
+                    'time' : store.time,
+                    'longitude' : store.longitude,
+                    'latitude' : store.latitude
+                }
+            ]
+        return JsonResponse({'store' : store_data}, status=200)
+
+class SearchView(View):
+    def get(self, request):
+        search = request.GET.get('search', '')
+        stores = Store.objects.filter(Q(name__contains=search)|Q(address__contains=search)).all()
+        search_data = [
+                {
+                    'id' : store.id,
+                    'name' : store.name,
+                    'address' : store.address,
+                    'phone' : store.phone,
+                    'time' : store.time
+                }for store in stores
+            ]
+
+        return JsonResponse({'stores' : search_data}, status=200)
+
